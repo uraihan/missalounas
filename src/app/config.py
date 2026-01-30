f5ded1a8fb5d1eb7883a67826a65e9bda78a0734
uraihan <umair.umer26@gmail.com>
2026-01-30 16:34:41 +0200

    changed postgres connection string to URI-style string

--- src/app/config.py
+++ src/app/config.py
@@ -1,4 +1,5 @@
 import os
+
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -9,13 +10,17 @@ else:
     db_type = os.getenv("DB_TYPE")
     db_user = os.getenv("DB_USER")
     db_name = os.getenv("DB_NAME")
-    db_string = f"dbname={db_name} user={db_user} host='localhost'"
 
-    if os.getenv("DB_PASSWORD"):
-        db_pass = os.getenv("DB_PASSWORD")
-        db_string += f" password={db_pass}"
+    db_pass = os.getenv("DB_PASSWORD", "postgres")
+    db_host = os.getenv("DB_HOST", "db")
+    db_port = os.getenv("DB_PORT", "5432")
 
-db_string = db_string
+    if db_type == "postgresql":
+        db_string = f"{db_type}://{db_user}:{db_pass}@{db_host}:{db_port}/{db_name}"
+    else:
+        db_string = f"dbname={db_name} user={db_user} host=db password={db_pass}"
+
+# db_string = db_string
 
 DEFAULT_CITY = "Tampere"
 DATE_FORMAT = "%d.%m.%Y"
