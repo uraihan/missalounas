11b995a2dfbd78a9f1f6009517e19b8360eb48d2
uraihan <umair.umer26@gmail.com>
2026-09-02 17:47:29 +0300

    finishing server-side page load logic that fetches menu data information from backend API

--- frontend/src/routes/+page.server.ts
+++ frontend/src/routes/+page.server.ts
@@ -1,13 +1,32 @@
-import { menuGetMenu } from "$lib/api/gen/default.ts";
+import { areasGetAreas, menuGetMenu } from "$lib/api/gen/default.ts";
+import { error } from "@sveltejs/kit";
 import type { PageServerLoad } from "./$types";
 
-export const load: PageServerLoad = async ({ params }) => {
-  const menuData = await menuGetMenu({
-    day: "monday",
-    city: "Tampere",
-    area: "Keskusta",
-    lang: "fi",
-  });
+export const load: PageServerLoad = async ({ url }) => {
+  // TODO: change these to be aware of client's geolocation
+  const city = url.searchParams.get("city") ?? "Tampere";
+  const area = url.searchParams.get("area") ?? "Hervanta";
+  const lang = url.searchParams.get("lang") ?? "en";
 
-  return { data: menuData.data };
+  const menuResp = await menuGetMenu({ city, area, lang });
+  if (menuResp.status !== 200) {
+    throw error(menuResp.status, "Failed to load menu");
+  }
+  const areaData = await areasGetAreas({ city });
+  if (areaData.status !== 200) {
+    throw error(areaData.status, "Failed to load areas");
+  }
+
+  const menuData = menuResp.data.map((day) => ({
+    ...day,
+    calendar_date: day.calendar_date.slice(0, 10),
+  }));
+
+  return {
+    areas: areaData.data,
+    weeklyMenu: menuData,
+    selectedCity: city,
+    selectedArea: area,
+    selectedLang: lang,
+  };
 };
