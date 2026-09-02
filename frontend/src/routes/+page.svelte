2d950c358d60252aab5e9cdb6396268cb3211b7a
uraihan <umair.umer26@gmail.com>
2026-09-02 17:48:09 +0300

    finishing the main page view

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,20 +1,57 @@
 <script lang="ts">
-  import { menuGetMenu } from "$lib/api/gen/default";
-  import Navbar from "$lib/components/components/Navbar.svelte";
+  import type { WeeklyMenu } from "$lib/api/gen/model/weeklyMenu.js";
+  import AreaTab from "$lib/components/AreaTab.svelte";
+  import Navbar from "$lib/components/Navbar.svelte";
   import * as Card from "$lib/components/ui/card";
 
   let { data } = $props();
+  let menuData: WeeklyMenu[] = $derived(data.weeklyMenu);
 
-  console.log(data);
+  // NOTE: reconsider how to deal with date and time consistently
+  let selectedDate = $state(new Date().toLocaleDateString("sv-SE"));
+
+  const dayMenu = $derived(
+    menuData.find((day) => day.calendar_date === selectedDate) ?? menuData[0],
+  );
 </script>
 
 <header>
-  <Navbar />
+  <Navbar
+    cities={data.cities}
+    {menuData}
+    selectedCity={data.selectedCity}
+    selectedLang={data.selectedLang}
+    bind:selectedDate
+  />
 </header>
-<main>
-  <Card.Root>
-    <Card.Header>
-      <Card.Title>Restaurant Name</Card.Title>
-    </Card.Header>
-  </Card.Root>
+<main class="mt-5 space-y-4">
+  <AreaTab areaList={data.areas} initialSelectedArea={data.selectedArea} />
+  <div class="grid grid-cols-4 gap-3 mt-2">
+    {#if dayMenu}
+      {#each dayMenu.restaurant_data as restaurant}
+        <!-- each menu data is grouped per calendar day. has to be deconstructed in page.server.ts -->
+        <Card.Root class="flex-1">
+          <Card.Header>
+            <Card.Title class="text-xl">{restaurant.restaurant_name}</Card.Title
+            >
+          </Card.Header>
+          <Card.Content class="flex flex-col gap-4">
+            {#each restaurant.menu_group_list as menu}
+              <div>
+                <h2 class="font-semibold">{menu.group_name}</h2>
+                {#each menu.menu_item_list as menuItem}
+                  <div class="flex justify-between gap-8 items-center my-1">
+                    <p>{menuItem.name}</p>
+                    <p class="text-end">{menuItem.diet}</p>
+                  </div>
+                {/each}
+              </div>
+            {:else}
+              <p>No Menu found</p>
+            {/each}
+          </Card.Content>
+        </Card.Root>
+      {/each}
+    {/if}
+  </div>
 </main>
