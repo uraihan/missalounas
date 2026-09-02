dc8109c0970b408a67d16ff79eaf2f4cb21b3b66
uraihan <umair.umer26@gmail.com>
2026-09-02 17:12:45 +0300

    reorganizing UI component libraries

---
+++ frontend/src/lib/components/Navbar.svelte
@@ -0,0 +1,117 @@
+<script lang="ts">
+  import { goto } from "$app/navigation";
+  import { page } from "$app/state";
+  import type { CitiesGetCities200Item, WeeklyMenu } from "$lib/api/gen/model";
+  import * as Select from "$lib/components/ui/select/index";
+  import { cn } from "$lib/utils";
+
+  const days = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+  ];
+
+  let {
+    cities,
+    menuData,
+    selectedLang,
+    selectedCity,
+    selectedDate = $bindable(),
+  }: {
+    cities: CitiesGetCities200Item[];
+    menuData: WeeklyMenu[];
+    selectedLang: string;
+    selectedCity: string;
+    selectedDate: string;
+  } = $props();
+  const triggerCity = $derived(
+    cities.find((c) => c.name === selectedCity)?.name ?? "Select a city",
+  );
+
+  function selectCity(city: string) {
+    selectedCity = city;
+    const params = new URLSearchParams(page.url.searchParams);
+    params.set("city", city);
+    params.delete("area");
+
+    goto(`?${params}`, { keepFocus: true, noScroll: true, replaceState: true });
+  }
+
+  // available language
+  const lang = ["EN", "FI"];
+  // let selectedLang = $state(navigator.language.split("-").at(0)?.toUpperCase());
+  const triggerLang = $derived(
+    lang.find((c) => c === selectedLang.toUpperCase()), // replace this with system language
+  );
+
+  // const today = Date.now();
+  // const date = new Date(today).toLocaleDateString("fi-FI");
+</script>
+
+<nav class="flex justify-center lg:justify-between">
+  <div class="flex flex-col sm:flex-row gap-6">
+    <div class="flex items-center">
+      <div class="items-center">
+        <a class="text-xl sm:text-2xl xl:text-3xl" href="#">
+          <strong>Missä Lounas??</strong>
+        </a>
+        <!-- <p class="text-center">{date}</p> -->
+      </div>
+    </div>
+
+    <div class="gap-3 items-center lg:flex">
+      {#each menuData as { calendar_date } (calendar_date)}
+        <button
+          class={cn(
+            "lg:text-lg",
+            calendar_date === selectedDate && "font-bold",
+          )}
+          onclick={() => (selectedDate = calendar_date)}
+        >
+          {new Date(calendar_date).toLocaleDateString("en-US", {
+            weekday: "long",
+          })}
+        </button>
+      {/each}
+    </div>
+  </div>
+
+  <div class="flex items-center gap-6">
+    <Select.Root
+      type="single"
+      name="selectedCity"
+      bind:value={selectedCity}
+      onValueChange={selectCity}
+    >
+      <Select.Trigger class="w-[180px]">{triggerCity}</Select.Trigger>
+      <Select.Content>
+        <Select.Group>
+          <Select.Label>Cities</Select.Label>
+          {#each cities as city}
+            <Select.Item value={city.name} label={city.name}>
+              {city.name}
+            </Select.Item>
+          {/each}
+        </Select.Group>
+      </Select.Content>
+    </Select.Root>
+
+    <Select.Root type="single" name="selectedLang" bind:value={selectedLang}>
+      <Select.Trigger class="w-[180px]">{triggerLang}</Select.Trigger>
+      <Select.Content>
+        <Select.Group>
+          <Select.Label>Language</Select.Label>
+          {#each lang as langs}
+            <Select.Item value={langs} label={langs}>
+              {langs}
+            </Select.Item>
+          {/each}
+        </Select.Group>
+      </Select.Content>
+    </Select.Root>
+  </div>
+</nav>
