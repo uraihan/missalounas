9ea101510be59ff557c178def2fac83bd34e291a
uraihan <umair.umer26@gmail.com>
2026-09-06 04:18:53 +0300

    removing ALTER command

--- db/migrations/20260903000000_add_default_area.sql
+++ db/migrations/20260903000000_add_default_area.sql
@@ -1,5 +1,4 @@
 --migrate:up
-ALTER TABLE cities ADD COLUMN default_area TEXT; --TODO: remove this in prod
 UPDATE cities SET default_area = 'Hervanta' WHERE name = 'Tampere';
 UPDATE cities SET default_area = 'Keskusta' WHERE name = 'Helsinki';
 UPDATE cities SET default_area = 'UTU Kampus' WHERE name = 'Turku';
